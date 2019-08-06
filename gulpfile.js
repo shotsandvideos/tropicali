91bbf3b7a60b8fdf199c075783ad97e30b3faef6
Antonio <bellu.antonio@gmail.com>
2019-08-06 18:10:20 +0200

    working out

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,17 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+ 
+sass.compiler = require('node-sass');
+
 function defaultTask(cb) {
     // place code for your default task here
+
+    gulp.task('sass', function () {
+        return gulp.src('assets/css/app.scss')
+          .pipe(sass().on('error', sass.logError))
+          .pipe(gulp.dest('app.css'));
+      });
+
     cb();
   }
   
