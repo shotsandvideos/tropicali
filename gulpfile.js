175379c3742af433e14318ba7920cce2aecd0b0e
Antonio <bellu.antonio@gmail.com>
2019-08-07 12:26:15 +0200

    optimized version

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,58 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+let cleanCSS = require('gulp-clean-css');
+var browserSync = require('browser-sync').create();
+var imagemin = require('gulp-imagemin');
 
 sass.compiler = require('node-sass');
 
+//GULP SASS + MINIFY CSS
 gulp.task('sass', function () {
-  return gulp.src('assets/css/*.scss')
+  return gulp.src('src/css/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('css'));
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('dist'))
+    .pipe(browserSync.stream())
 });
 
-gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+//INJECT HTML
+
+gulp.task("html", function(){
+  return gulp.src("src/index.html")
+  .pipe(gulp.dest("dist"))
+
+})
+
+//INJECT FONTS
+
+gulp.task("fonts", function (){
+  return gulp.src("src/fonts/*")
+  .pipe(gulp.dest("dist/fonts"))
+})
+
+//INJECT IMAGES
+
+gulp.task("images", function(){
+  return gulp.src("src/img/*")
+  .pipe(imagemin())
+  .pipe(gulp.dest("dist/img"))
+})
+
+//GULP WATCH
+gulp.task('watch', function () {
+
+  browserSync.init({
+    server: {
+        baseDir: "dist"
+    }
 });
+
+  gulp.watch("src/css/*.scss", gulp.series('sass', 'html', 'fonts', 'images', 'watch'));
+  gulp.watch("src/*.html", gulp.series('sass', 'html', 'fonts', 'images', 'watch')).on('change', browserSync.reload);
+  gulp.watch("src/fonts/*", gulp.series('sass', 'html', 'fonts', 'images', 'watch')).on('change', browserSync.reload);
+  gulp.watch("src/img/*", gulp.series('sass', 'html', 'fonts', 'images', 'watch')).on('change', browserSync.reload);
+});
+
+
+gulp.task('default', gulp.series('sass', 'html', 'fonts', 'images', 'watch'));
