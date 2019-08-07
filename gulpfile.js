5d8a5fc025e64076de95854951a4a3a55204f708
Antonio <bellu.antonio@gmail.com>
2019-08-07 09:57:00 +0200

    sass 2

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,16 @@
 'use strict';
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
- 
+
 sass.compiler = require('node-sass');
 
-    gulp.task('sass', function () {
-        return gulp.src('assets/css/app.scss')
-          .pipe(sass().on('error', sass.logError))
-          .pipe(gulp.dest('app.css'));
-      });
+gulp.task('sass', function () {
+  return gulp.src('assets/css/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
